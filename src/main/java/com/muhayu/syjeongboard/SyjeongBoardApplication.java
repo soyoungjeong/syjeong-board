11d1f41c79788c8e37868f1bf852e122b883f288
syjeong-muhayu <45409657+syjeong-muhayu@users.noreply.github.com>
2018-11-30 11:25:00 +0900

    #3496 spring-boot test

---
+++ src/main/java/com/muhayu/syjeongboard/SyjeongBoardApplication.java
@@ -0,0 +1,12 @@
+package com.muhayu.syjeongboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyjeongBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyjeongBoardApplication.class, args);
+    }
+}
